8e57a4b49dcc05ee1ff6f29e7981a98159210c1e
jgu2 <jianli.gu@nrel.gov>
2024-09-12 15:38:57 -0600

    Update requirement rendering

--- src/pages/ProjectOverviewRequirements.js
+++ src/pages/ProjectOverviewRequirements.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Table from "react-bootstrap/Table";
 
+
 const ProjectOverviewRequirements = ({requirements}) => {
   return (
     <Table striped bordered hover>
@@ -14,7 +15,13 @@ const ProjectOverviewRequirements = ({requirements}) => {
           {Object.entries(requirements).map(([key, value]) => (
             <tr key={key}>
               <td>{key.replace('_', ' ').replace(/\b\w/g, char => char.toUpperCase())}</td>
-              <td>{Array.isArray(value) ? value.join(', ') : value}</td>
+              <td>
+              {Array.isArray(value)
+                ? value.join(', ') // If it's an array, join the elements
+                : typeof value === 'object' && value !== null
+                ? JSON.stringify(value) // If it's an object, convert it to a string
+                : value} {/* For everything else, render as-is */}
+            </td>
             </tr>
           ))}
         </tbody>
