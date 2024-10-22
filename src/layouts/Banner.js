ca00327c17b64230d588e3ccc583c004d40dcbfc
jgu2 <jianli.gu@nrel.gov>
2024-10-22 11:00:15 -0600

    Update with pipes new logos

--- src/layouts/Banner.js
+++ src/layouts/Banner.js
@@ -11,7 +11,15 @@ const SiteBanner = () => {
     <Container className="banner">
       <Row>
         <Col className="banner-pipes-name" md={9} sm={8} xs={8}>
-          <a href="/">PIPES</a>
+          <a href="/">
+            <Image
+              className="rounded"
+              src="/images/NREL-PIPES-Logo-FullColor.png"
+              alt="PIPES"
+              style={{ width: "28%" }}
+              fluid
+            />
+          </a>
         </Col>
         <Col className="banner-nrel-container" md={3} sm={4} xs={4}>
           <Image className="banner-nrel-image" src="./images/nrel-logo@2x-01.png" alt="NREL" fluid />
