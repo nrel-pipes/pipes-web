a12887b2a216cd0e59dcff0dbcfe41d6dd5fbe69
jeisenma <Jordan.Eisenman@nrel.gov>
2025-01-10 10:14:57 -0700

    start to docs

---
+++ src/pages/Docs.js
@@ -0,0 +1,15 @@
+import Container from "react-bootstrap/Container";
+
+import "reactflow/dist/style.css";
+import "./PageStyles.css";
+import "./ReactflowStyles.css";
+
+const Docs = () => {
+  return (
+    <Container>
+      <div>hello</div>
+    </Container>
+  );
+};
+
+export default Docs;
